e3c46dba6525f4836df26b740a0750fcc7f79dd2
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2022-07-15 11:49:24 +0300

    Create Remove Duplicates from sorted list.cpp
    A2SV Challenge

---
+++ Remove Duplicates from sorted list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL)
+            return head;
+        if(head->next == NULL)
+            return head;
+        
+        ListNode *temp = head;
+        ListNode *temp2 = head->next;
+        
+        while(temp2 != NULL)
+        {
+            if(temp->val == temp2->val)
+            {
+                temp->next = temp2->next;
+                delete temp2;
+                temp2 = temp->next;
+            }
+            else
+            {
+                temp = temp2;
+                temp2 = temp2->next;
+            }  
+        }
+        return head;
+    }
+};
