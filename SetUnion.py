939d2fe3a86e31f9b02e21b2d8b9c8822757f325
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2022-12-30 23:32:24 +0300

    HackerRank set-union

---
+++ SetUnion.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+set1Size = int(input())
+set1 = set(input().split())
+
+set2Size = int(input())
+set2 = set(input().split())
+
+print(len(set1|set2))
