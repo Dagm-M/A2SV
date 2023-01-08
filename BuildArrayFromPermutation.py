bd681ff9d825ee1ef494d3ff9458992a3fda2aba
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-01-08 23:39:23 +0300

    LeetCode 1920.

---
+++ BuildArrayFromPermutation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        permutation = []
+
+        for num in nums:
+            permutation.append(nums[num])
+
+        return permutation
