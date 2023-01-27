21d4f12795909f54f25958d2bb133c9a4e2b19b5
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-01-27 23:29:10 +0300

    LeetCode 1365.

---
+++ HowManyNumbersAreSmallerThanTheCurrentNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        n = sorted(nums)
+
+        for i in range(len(nums)):
+            nums[i] = n.index(nums[i])
+        
+        return nums
