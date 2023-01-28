5b4f73c172df35953dd27df75b30b114374c0f96
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-01-28 12:12:48 +0300

    LeetCode 2089.

---
+++ FindTargetIndicesAfterSortingArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        index = []
+        for i,num in enumerate(nums):
+            if num == target:
+                index.append(i)
+
+        return index
