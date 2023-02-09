1116eff170c58d969d0e995aed10b84c9a182264
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-02-09 12:44:31 +0300

    LeetCode 633.

---
+++ SumOfSquareNumbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int(sqrt(c))
+
+        while left <= right:
+            if pow(left,2) + pow(right,2) < c:
+                left += 1
+            elif pow(left,2) + pow(right,2) > c:
+                right -= 1
+            else:
+                return True
+
+        return False
+
+        
+        
