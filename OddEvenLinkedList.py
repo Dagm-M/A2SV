a1782154971df222e838119693565ea0ca8e86c6
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-02-21 09:32:00 +0300

    Update OddEvenLinkedList.py

--- OddEvenLinkedList.py
+++ OddEvenLinkedList.py
@@ -6,41 +6,71 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        temp = head
-        dummy = None
+#         temp = head
+#         dummy = None
+
+#         if head == None:
+#             return None
+
+
+#         while temp != None:
+#             if dummy == None:
+#                 temp2 = ListNode(temp.val,None)
+#                 dummy = temp2
+#             else:
+#                 temp3 = ListNode(temp.val,None)
+#                 temp2.next = temp3
+#                 temp2 = temp2.next
+
+#             temp = temp.next
+#             if temp == None:
+#                 break
+#             temp = temp.next
+
+
+#         temp = head.next
+
+#         while temp != None:
+
+#             temp3 = ListNode(temp.val,None)
+#             temp2.next = temp3
+#             temp2 = temp2.next
+
+#             temp = temp.next
+#             if temp == None:
+#                 break
+#             temp = temp.next
+
+#         return dummy
 
         if head == None:
-            return None
+            return head
+        elif head.next == None:
+            return head
 
+        odd = head
+        even = head.next
 
-        while temp != None:
-            if dummy == None:
-                temp2 = ListNode(temp.val,None)
-                dummy = temp2
-            else:
-                temp3 = ListNode(temp.val,None)
-                temp2.next = temp3
-                temp2 = temp2.next
+        tempOdd = odd
+        tempEven = even
 
-            temp = temp.next
-            if temp == None:
+        while tempOdd != None:
+            tempOdd.next = tempEven.next
+
+            if tempOdd.next == None:
                 break
-            temp = temp.next
 
+            tempOdd = tempOdd.next
 
-        temp = head.next
+            tempEven.next = tempOdd.next
 
-        while temp != None:
-
-            temp3 = ListNode(temp.val,None)
-            temp2.next = temp3
-            temp2 = temp2.next
-
-            temp = temp.next
-            if temp == None:
+            if tempEven.next == None:
                 break
-            temp = temp.next
 
-        return dummy
+            tempEven = tempEven.next
+
+        tempOdd.next = even
+
+        return head   
 
         
