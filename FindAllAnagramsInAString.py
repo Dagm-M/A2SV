ad358e1fef617ad9949dee2fd14c866f8c993e9f
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-02-28 15:18:15 +0300

    LeetCode 438.

---
+++ FindAllAnagramsInAString.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        
+        l = len(s)
+        k = len(p)
+        p_counter = defaultdict(int)
+        s_counter = defaultdict(int)
+        count = []
+        
+        
+        for i in p :
+            p_counter[i]+=1
+        right = 0 
+        left = 0
+        if l < k:
+            return []
+        while right < k :
+            s_counter[s[right]]  += 1
+            right+=1
+            if s_counter == p_counter:
+                count.append(left)
+        
+        while right < l:
+            s_counter[s[right]]+=1
+            s_counter[s[left]] -= 1
+            if  s_counter[s[left]] == 0:
+                del  s_counter[s[left]]
+
+            left+=1 
+            if s_counter == p_counter:
+                count.append(left)
+               
+            right+=1 
+           
+        return count
