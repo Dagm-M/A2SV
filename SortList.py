44543af3ed6b4a7f5f0e1f5da268dbb957686118
Dagm-M <86826089+Dagm-M@users.noreply.github.com>
2023-03-10 10:00:43 +0300

    LeetCode 148.

---
+++ SortList.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        newHead = ListNode()
+        temp = newHead
+
+        while list1 != None and list2 != None:
+            if list1.val > list2.val:
+                temp2 = ListNode(list2.val)
+                temp.next = temp2
+                temp = temp2
+                list2 = list2.next
+            else:
+                temp2 = ListNode(list1.val)
+                temp.next = temp2
+                temp = temp2
+                list1 = list1.next
+
+
+        while list1 != None:
+            temp2 = ListNode(list1.val)
+            temp.next = temp2
+            temp = temp2
+            list1 = list1.next
+
+        while list2 != None:
+            temp2 = ListNode(list2.val)
+            temp.next = temp2
+            temp = temp2
+            list2 = list2.next
+
+        newHead = newHead.next
+
+        return newHead
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or head.next == None:
+            return head
+
+        dummy = ListNode()
+        dummy.next = head
+        fast = dummy
+        slow = dummy
+
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.mergeTwoLists(left,right)
